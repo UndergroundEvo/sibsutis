338618c5df1f2b5479a93aaeff9d29036b81c291
Miron <119624978+UndergroundEvo@users.noreply.github.com>
2022-12-02 21:17:18 +0700

    Add files via upload

---
+++ 2 Grade/3 Semester/OOP/Lab3/Лаб3_SDL/Лаб3/Лаб3.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <SDL.h>
+#include <time.h>
+using namespace std;
+const int size_array = 100;
+
+class tPoint {
+private:
+	const char* title = "Lab_3";
+	const int SCREEN_WIDTH = 640;
+	const int SCREEN_HEIGHT = 480;
+	const int DOT_WIDTH = 20;
+	const int DOT_HEIGHT = 20;
+	const int DOT_VEL = 10;
+	int mPosX=0, mPosY=0, mVelX=0, mVelY=0;
+	SDL_Window* win = NULL;
+	SDL_Renderer* ren = NULL;
+	SDL_Surface* bmp = NULL;
+	SDL_Texture* tex = NULL;
+public:
+	void Create_RenderWindow() {
+		win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	}
+	void SetBackground() {
+		bmp = SDL_LoadBMP("background.bmp");
+		tex = SDL_CreateTextureFromSurface(ren, bmp);
+		SDL_FreeSurface(bmp);
+		SDL_RenderClear(ren);
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		SDL_RenderPresent(ren);
+	}
+	void Destroy_Everything() {
+		SDL_DestroyTexture(tex);
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		SDL_Quit();
+	}
+	void Dots_Move()
+	{
+		mPosX += mVelX;
+
+		if ((mPosX < 0) || (mPosX + DOT_WIDTH > SCREEN_WIDTH))
+		{
+			mPosX -= mVelX;
+		}
+	}
+	//void Dots_Render()
+	//	{
+	//		//Show the dot
+	//		gDotTexture.Dots_Render(mPosX, mPosY);
+	//	}
+};
+int SDL_main(int argc, char* argv[]) 
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+
+	bool quit = false;
+	SDL_Event event;
+	tPoint arr[size_array],open;
+	int show_time = 10000;
+	
+	open.Create_RenderWindow();
+	open.SetBackground();
+
+	SDL_Delay(show_time);
+	
+	return 0;
+}
---
+++ 2 Grade/3 Semester/OOP/Lab3/Пример/Moving/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Moving
+{
+    public partial class Form1 : Form
+    {
+        Ball[] ball;
+        int n;
+       
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            n = 100;
+            ball = new Ball[n];
+            for (int i = 0; i < n; i++)
+            {
+                ball[i] = new Ball(this);
+            }
+
+           
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            
+            for (int i = 0; i < ball.Length; i++)
+            {
+                
+                ball[i].Paint(g);
+            }
+            
+           
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                ball[i].Th.Abort();
+            }
+            //ball.Th.Abort();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < n; i++)
+            {
+                ball[i].Sss();  
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                ball[i].Stop();
+            }
+        }
+
+   
+
+
+
+    }
+}
