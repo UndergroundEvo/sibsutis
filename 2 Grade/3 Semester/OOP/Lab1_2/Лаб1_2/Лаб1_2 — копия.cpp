f248d89733899856301422d964c08ace60f67066
Miron <119624978+UndergroundEvo@users.noreply.github.com>
2022-12-02 21:11:48 +0700

    Add files via upload

---
+++ 2 Grade/3 Semester/OOP/Lab1_2/Лаб1_2/Лаб1_2 — копия.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int** genRandMatrix(int size, int maxValue) 
+{
+	int i;
+	int** matrix = new int* [size];
+	for (i = 1; i <= size;i++) {
+		int r = rand() % 10 + 1;
+		*matrix[i] = r;
+		matrix[i][1] = r;
+		for (int j = 2; j < matrix[i][1];j++) {
+			matrix[i][j] = rand() % maxValue + 1;
+		}
+	}
+	matrix[1][1] = i;
+	return matrix;
+}
+void print(int** matrix) {
+	for (int i = 1; i < matrix[1][1];i++) {
+		cout << matrix[i][0] << ": ";
+		for (int j = 1; j < matrix[i][0];j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+	delete matrix;
+}
+
+int main() {
+	srand(time(0));
+	int size = 10;
+	//int size = rand() % 10;
+	int maxValue = 100;
+	int** matrix = genRandMatrix(size, maxValue);
+	print (matrix);}
